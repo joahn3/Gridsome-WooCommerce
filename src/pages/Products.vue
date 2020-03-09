3df4bacde4a0604ba0d3b1abc2ce6f54a16b39ae
Scott Kennedy <scottyzen@gmail.com>
2020-03-09 21:21:59 +0000

    Included wp-images

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -10,9 +10,9 @@
       >
         Goooooo!
       </button>
-      <ul class="flex flex-wrap justify-between">
+      <ul class="flex flex-wrap justify-between -mx-8">
         <li
-          class="w-full md:w-1/4  p-4 text-center"
+          class="w-1/2 md:w-1/3 lg:w-1/4 p-4 text-center"
           v-for="{ node } in $page.allWooCommerceProduct.edges"
           :key="node.id"
         >
@@ -29,7 +29,7 @@
 
 <page-query>
 query Products ($page: Int) {
-  allWooCommerceProduct (sortBy: "date_created" page: $page, perPage: 24) @paginate {
+  allWooCommerceProduct (sortBy: "date_created" filter: {price: {ne: ""}} page: $page, perPage: 12) @paginate {
     pageInfo{
       currentPage
       totalPages
@@ -71,7 +71,7 @@ export default {
 <style>
 .product-image {
   width: 100%;
-  height: 300px;
+  height: 240px;
   object-fit: contain;
   max-width: 300px;
 }
