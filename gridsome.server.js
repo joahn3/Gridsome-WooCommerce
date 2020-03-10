5986fd4e0aa569f7dc699d74a45cdde8924c1f14
Scott Kennedy <scottyzen@gmail.com>
2020-03-10 22:22:39 +0000

    Allow more than 100 products

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,10 +1,3 @@
-// Server API makes it possible to hook into various parts of Gridsome
-// on server-side and add custom data to the GraphQL data layer.
-// Learn more: https://gridsome.org/docs/server-api/
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
-
 const consumerKey = 'ck_4d785418513420422fc9f67122d4d3a2b4b4e42a';
 const consumerSecret = 'cs_6b623011691f42de6aa1d09ec4b5310e2cc98aba';
 const siteUrl = 'https://thestoveshop.ie';
@@ -20,19 +13,15 @@ const WooCommerce = new WooCommerceRestApi({
 });
 
 module.exports = function (api) {
-  
   api.loadSource(async ({ addCollection }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
     const products = addCollection("WooCommerceProduct");
-    const { data } = await WooCommerce.get("products", { per_page: 100 });
-    console.log(data);
-    
-    for (const item of data) {
+    let final = [];
+    for (let step = 0; step < 5; step++) {
+      let { data } = await WooCommerce.get("products", { per_page: 100, offset: step*100});
+      final = [...final, ...data];
+    }
+    for (const item of final) {
        products.addNode({ ...item });
     }
-})
-
-  api.createPages(({ createPage }) => {
-    // Use the Pages API here: https://gridsome.org/docs/pages-api/
   })
 }
