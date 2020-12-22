ac58da16004721c3dad3436e9bea7f517f3abaa0
Ionut - Iulian Francisc <joahn3@protonmail.ch>
2020-12-22 04:00:52 +0200

    Update gridsome.server.js

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,6 +1,6 @@
 const consumerKey = 'ck_4d785418513420422fc9f67122d4d3a2b4b4e42a';
 const consumerSecret = 'cs_6b623011691f42de6aa1d09ec4b5310e2cc98aba';
-const siteUrl = 'https://thestoveshop.ie';
+const siteUrl = 'https://chicroumaine.com';
 
 
 const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
